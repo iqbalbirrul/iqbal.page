d110a3e82eddc0772ed5f9eb8fea27c3ee63c064
Iqbal Birrul Walidain <iqbal@app-server.tel.my.id>
2021-06-13 02:25:11 +0700

    ubah host live

--- live.sh
+++ live.sh
@@ -3,4 +3,4 @@ echo -e "\033[0;32mPreview Jekyll Site...\033[0m"
 
 # Live Jekyll
 #bundle exec jekyll serve --port 4000 --host node.tel.my.id --watch
-setsid bundle exec jekyll serve --port 4000 --host 192.168.43.2 --watch --force_polling &>/dev/null </dev/null &
+setsid bundle exec jekyll serve --port 4000 --host 103.41.205.205 --watch --force_polling &>/dev/null </dev/null &

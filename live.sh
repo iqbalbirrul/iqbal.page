82524f2bfdfe5a4ff990c27585be060640e754c7
Plunderer <iqbalbirrul@gmail.com>
2021-06-16 19:01:30 +0700

    ubah host live

--- live.sh
+++ live.sh
@@ -2,5 +2,5 @@
 echo -e "\033[0;32mPreview Jekyll Site...\033[0m"
 
 # Live Jekyll
-#bundle exec jekyll serve --port 4000 --host node.tel.my.id --watch
-setsid bundle exec jekyll serve --port 4000 --host 103.41.205.205 --watch --force_polling &>/dev/null </dev/null &
+#bundle exec jekyll serve --port 4000 --host localhost --watch
+setsid bundle exec jekyll serve --port 4000 --host 192.168.10.2 --watch --force_polling &>/dev/null </dev/null &
